4ea9f0e6dbbf435cdb5a34a8a635633ebe2419a2
Kamil Wlazło <kwlazlo10@gmail.com>
2019-04-11 23:00:01 +0200

    Change temperature on click function added

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,13 +1,11 @@
 window.addEventListener('load', ()=>{
     let long;
     let lat;
-    let temperatureDescription = document.querySelector(
-        ".temperature-description");
-    let temperatureDegree = document.querySelector(
-        ".temperature-degree");
-    let locationTimezone = document.querySelector(
-        ".location-timezone");
-
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let locationTimezone = document.querySelector(".location-timezone");
+    let temperatureSection = document.querySelector(".temperature");
+    const temperatureSpan = document.querySelector(".temperature span");
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position=>{
@@ -27,8 +25,21 @@ window.addEventListener('load', ()=>{
                 temperatureDegree.textContent = temperature;
                 temperatureDescription.textContent = summary;
                 locationTimezone.textContent = data.timezone;
+                //Formula for celsius
+                let celsius = (temperature - 32) * (5/9);
                 //Set Icon
                 setIcons(icon, document.querySelector(".icon"));
+                //Change temperature to Celsius/Farenheit
+                temperatureSection.addEventListener('click', ()=>{
+                    if(temperatureSpan.textContent === "F"){
+                        temperatureSpan.textContent = "C";
+                        temperatureDegree.textContent = Math.floor(celsius);
+                    } else{
+                        temperatureSpan.textContent = "F";
+                        temperatureDegree.textContent = temperature;
+                    }
+                })
+                
             });
         });
     }
